d78f3eff80d4b5f95af042f7b0f6eddab598a6c2
Zopanix <zopanix@gmail.com>
2018-03-16 10:34:09 -0400

    Bump cookbook version and set ubuntu version limit

--- metadata.rb
+++ metadata.rb
@@ -4,10 +4,10 @@ maintainer       'FXinnovation'
 maintainer_email 'cloudsquad@fxinnovation.com'
 description      'Installs and configures pam-radius'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 chef_version     '>= 12.1' if respond_to?(:chef_version)
 issues_url       'https://bitbucket.org/fxadmin/public-common-cookbook-pam_radius/issues'
 source_url       'https://bitbucket.org/fxadmin/public-common-cookbook-pam_radius'
 supports         'redhat',  '>= 6.0'
 supports         'centos',  '>= 6.0'
-supports         'ubuntu'
+supports         'ubuntu',  '>= 14.04'
